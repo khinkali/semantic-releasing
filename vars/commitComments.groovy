52382208c143c23a526add17cfe7ce882148bb5e
Robert Brem <robert.brem@isceco.admin.ch>
2018-02-07 18:16:24 +0100

    try to read last github release

--- vars/commitComments.groovy
+++ vars/commitComments.groovy
@@ -1,6 +1,10 @@
 #!/usr/bin/env groovy
 
 def call() {
+    def json = sh 'curl https://api.github.com/repos/khinkali/sink/releases/latest'
+    def data = readJSON text: $ { json }
+    echo "tag: ${data.tag_name}"
+
     def commitHistoryText = sh(
             script: 'git log `git describe --tags --abbrev=0`..HEAD --oneline',
             returnStdout: true
@@ -10,14 +14,11 @@ def call() {
     def array = commitHistoryText.split('\n')
     for (def i = 0; i < array.size(); i++) {
         def entry = array[i]
-        echo "entry: ${entry}"
         def startIndex = entry.indexOf(']')
-        echo "startIndex: ${startIndex}"
         if (startIndex == -1) {
             continue
         }
         allComments << entry.substring(startIndex + 1).trim()
-        echo "allComments: ${allComments}"
     }
 
     return allComments
