db0ba6a57e4a472cddc08274ce4650d4f15c74af
Robert Brem <robert.brem@isceco.admin.ch>
2018-02-07 17:55:12 +0100

    return all commit comments for release notes

---
+++ vars/commitComments.groovy
@@ -0,0 +1,21 @@
+#!/usr/bin/env groovy
+
+def call() {
+    def commitHistoryText = sh(
+            script: 'git log `git describe --tags --abbrev=0`..HEAD --oneline',
+            returnStdout: true
+    ).trim()
+
+    def allComments = []
+    def array = commitHistoryText.split('\n')
+    for (def i = 0; i < array.size(); i++) {
+        def entry = array[i]
+        def startIndex = entry.indexOf(']')
+        if (startIndex == -1) {
+            continue
+        }
+        allComments << entry.substring(startIndex + 1).trim()
+    }
+
+    return allComments
+}
