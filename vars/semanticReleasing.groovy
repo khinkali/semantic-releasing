73e0fd608e7f5b195871fcae321cba2b4e68f230
Robert Brem <robert.brem@isceco.admin.ch>
2018-02-07 13:49:48 +0100

    make keywords configurable

--- vars/semanticReleasing.groovy
+++ vars/semanticReleasing.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def call(String[] majorKeywords = ['API'], String[] minorKeywords = ['FEAT']) {
+def call(var majorKeywords = ['API'], var minorKeywords = ['FEAT']) {
     def commitHistoryText = sh(
             script: "git log `git describe --tags --abbrev=0`..HEAD --oneline",
             returnStdout: true
