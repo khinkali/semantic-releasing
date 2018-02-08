e753036c57b98eee90d76bbdc744feca7431753c
Robert Brem <robert.brem@isceco.admin.ch>
2018-02-08 06:31:02 +0100

    use parameter for GitHub username and repository

--- vars/commitComments.groovy
+++ vars/commitComments.groovy
@@ -1,8 +1,8 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(String userName, String repositoryName) {
     def latestReleaseJson = sh(
-            script: 'curl https://api.github.com/repos/khinkali/sink/releases/latest',
+            script: "curl https://api.github.com/repos/${userName}/${repositoryName}/releases/latest",
             returnStdout: true
     ).trim()
     def data = readJSON text: "${latestReleaseJson}"
@@ -15,25 +15,25 @@ def call() {
     def allComments = []
     def array = commitHistoryText.split('\n')
     for (def i = 0; i < array.size(); i++) {
-        def entry = array[i]
+        String entry = array[i]
         def startIndex = entry.indexOf(']')
         if (startIndex == -1) {
             continue
         }
         entry = entry.substring(startIndex + 1).trim().replaceAll("\\\\n", "[NL]").replaceAll("\'", "[SINGLE QUOTE]").replaceAll("\"", "[DOUBLE QUOTE]")
-        entry = insertIssueLink(entry)
+        entry = insertIssueLink(entry, userName, repositoryName)
         allComments << entry
     }
 
     return allComments
 }
 
-def insertIssueLink(def entry) {
+def insertIssueLink(String entry, String userName, String repositoryName) {
     def startIndex = entry.indexOf('[')
     def endIndex = entry.indexOf(']')
     if (startIndex == -1 || endIndex == -1) {
         return entry
     }
     def issueNumber = entry.substring(startIndex + 1, endIndex).trim().toInteger()
-    return entry.substring(0, startIndex) + "[" + entry.substring(startIndex, endIndex + 1) + "](https://github.com/khinkali/sink/issues/" + issueNumber + ")" + entry.substring(endIndex + 1)
+    return entry.substring(0, startIndex) + "[" + entry.substring(startIndex, endIndex + 1) + "](https://github.com/${userName}/${repositoryName}/issues/" + issueNumber + ")" + entry.substring(endIndex + 1)
 }
