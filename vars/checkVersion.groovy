767e7923725855268d9826a9218fe2e4c05298fd
Robert Brem <robert.brem@isceco.admin.ch>
2018-02-12 09:44:28 +0100

    added checkVersion

---
+++ vars/checkVersion.groovy
@@ -0,0 +1,16 @@
+#!/usr/bin/env groovy
+
+def call(String version, String healthUrl, Integer sleepInSeconds = 1, Integer timeoutInSeconds = 2) {
+    def versionText = sh(
+            script: "curl ${healthUrl} --max-time ${timeoutInSeconds}",
+            returnStdout: true
+    ).trim()
+    while (versionText != version) {
+        sleep "${sleepInSeconds}"
+        echo "still waiting - version is ${versionText} and should be ${version}"
+        versionText = sh(
+                script: "curl ${healthUrl} --max-time ${timeoutInSeconds}",
+                returnStdout: true
+        ).trim()
+    }
+}
