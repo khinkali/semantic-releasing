253874b9e1d42e5aec55587814e4f01bebcdeb88
Robert Brem <robert.brem@isceco.admin.ch>
2018-02-12 09:52:44 +0100

    correct hard coded typo

--- vars/gitHubRelease.groovy
+++ vars/gitHubRelease.groovy
@@ -5,5 +5,5 @@ def call(String version, String userName, String repository, String accessToken)
     for (def commitMessage : commitComments(userName, repository)) {
         RELEASE_NOTES += "* ${commitMessage}\\n"
     }
-    sh "curl --data '{\"tag_name\": \"${version}\",\"target_commitish\": \"master\",\"name\": \"${version}\",\"body\": \"${RELEASE_NOTES}\",\"draft\": false,\"prerelease\": false}' https://api.github.com/repos/khinkali/sink/releases?access_token=${accessToken}"
+    sh "curl --data '{\"tag_name\": \"${version}\",\"target_commitish\": \"master\",\"name\": \"${version}\",\"body\": \"${RELEASE_NOTES}\",\"draft\": false,\"prerelease\": false}' https://api.github.com/repos/${userName}/${repository}/releases?access_token=${accessToken}"
 }
